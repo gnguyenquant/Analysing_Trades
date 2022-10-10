b09a17e53d468192878fb1ae43da5f3b26dcd928
Giang Truong Nguyen <74754581+giangnguyenquantum@users.noreply.github.com>
2022-10-10 20:06:28 +0800

    Update personal_function.py

--- personal_function.py
+++ personal_function.py
@@ -7,8 +7,8 @@ import numpy as np
 
 #This function is to enter the API key.
 def client():
-    apikey='MZoUNKZin6hLFAaPtMyi0GrZXjz8ca55OL4Qlmys3YuUOSzS6fdtW97WvlKzs5yA'
-    secret='QuIoCo9pTkb8hw1ktIXZoxtPKnG9e2kXYCLy05c876aHqA4NL1T4s5ZAWtD2q9y4'
+    apikey=''
+    secret=''
     return Client(apikey, secret)
 
 #This function is to take the historical data.
